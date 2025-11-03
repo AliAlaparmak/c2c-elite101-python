92ff424a1e7cb43aa92b99cafd859fd62611d14f
Ali Alaparmak <139662554+AliAlaparmak@users.noreply.github.com>
2025-11-03 16:58:56 -0600

    Updated conversation flow

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 
-"""
-    Welcome to Elite 101 this program is a starter for your chatbot project.
-    The starter prompts the user to enter their name and then greets them with a personalized message.
-
-    Functions:
-        get_user_name(): Prompts the user to enter their name and returns it.
-        greet_user(name): Prints a greeting message using the provided name.
-        main(): Main function that orchestrates the user input and greeting process.
-
-    Execution:
-        When the script is run directly (not imported as a module), it will execute the main() function.
-"""
+#Example of how Database would look like.
+# Would ideally have more entires and be updatable dynamically.
 database = [
     ["ORD001", "Ali Alaparmak", "Out for delivery", "Dallas, TX", "ETA: 20 minutes"]
 ]
